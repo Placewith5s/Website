d0f27002f808fc7f308447b8292cb92c7d3f2149
Placewith5s <132140520+Placewith5s@users.noreply.github.com>
2023-11-15 17:02:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//about us function
+// About us function
 function toggleAboutUs() {
-    var aboutUsSection = document.getElementById("About-Us");
+    let aboutUsSection = document.getElementById("About-Us");
     if (aboutUsSection.style.display === "none" || aboutUsSection.style.display === "") {
         aboutUsSection.style.display = "block";
     } else {
@@ -8,35 +8,39 @@ function toggleAboutUs() {
     }
 }
 
-//Dark mode, and Light mode
+// Dark mode and Light mode
 function setDarkTheme() {
-    
     document.body.classList.add('dark-theme');
 }
 
 function setLightTheme() {
-    
     document.body.classList.remove('dark-theme');
 }
 
+// Check if dark mode is preferred and set the theme accordingly
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     setDarkTheme();
 } else {
     setLightTheme();
 }
 
-//Menu icon function
-document.getElementById('menuIcon').addEventListener('click', function() {
-    var drawer = document.getElementById('drawer');
+// Menu icon function
+document.getElementById('menuIcon').addEventListener('click', function () {
+    let drawer = document.getElementById('drawer');
     drawer.style.display = (drawer.style.display === 'block') ? 'none' : 'block';
 });
 
-document.getElementById('aboutUsLink').addEventListener('click', function() {
+document.getElementById('aboutUsLink').addEventListener('click', function () {
     document.getElementById('drawer').style.display = 'none';
     document.getElementById('About-Us').style.display = 'block';
 });
 
-//Hide function
-document.getElementById('hideAboutUs').addEventListener('click', function() {
+document.getElementById('contact-Us-Link').addEventListener('click', function () {
+    document.getElementById('drawer').style.display = 'none';
+    document.getElementById('contact-Us-Link').style.display = 'block';
+});
+
+// Hide function
+document.getElementById('hideAboutUs').addEventListener('click', function () {
     document.getElementById('About-Us').style.display = 'none';
 });
