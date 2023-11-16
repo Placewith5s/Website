4bf024acbe303800f2f46f5234f37982484ef04b
Placewith5s <132140520+Placewith5s@users.noreply.github.com>
2023-11-16 21:35:36 +0100

    Update privacyp.js

--- privacyp.js
+++ privacyp.js
@@ -1,8 +1,27 @@
 function toggleDarkMode() {
-    let contactMain = document.getElementById("policy");
-    contactMain.classList.toggle("dark-mode");
+    let policyMain = document.getElementById("policy");
+    policyMain.classList.toggle("dark-mode");
+}
+
+function handleColorSchemeChange(event) {
+    if (event.matches) {
+        toggleDarkMode();
+    }
 }
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     toggleDarkMode();
 }
+
+const colorSchemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+colorSchemeMediaQuery.addEventListener("change", handleColorSchemeChange);
+
+window.addEventListener("beforeunload", function () {
+    colorSchemeMediaQuery.removeEventListener("change", handleColorSchemeChange);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    if (colorSchemeMediaQuery.matches) {
+        toggleDarkMode();
+    }
+});
