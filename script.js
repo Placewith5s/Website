18a54b66eec57b9520011e4ca7705da37c79708b
Placewith5s <132140520+Placewith5s@users.noreply.github.com>
2023-11-21 19:35:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,55 @@
-// About us function
+// Function to toggle the display of the 'About Us' section
 function toggleAboutUs() {
-    // Get the About Us section element
     let aboutUsSection = document.getElementById("About-Us");
-
-    // Toggle the display of the About Us section
-    if (aboutUsSection.style.display === "none" || aboutUsSection.style.display === "") {
-        aboutUsSection.style.display = "block";
-    } else {
-        aboutUsSection.style.display = "none";
-    }
+    aboutUsSection.style.display = (aboutUsSection.style.display === "none" || aboutUsSection.style.display === "") ? "block" : "none";
 }
 
-// Dark mode and Light mode functions
+// Function to set the dark theme
 function setDarkTheme() {
-    // Add the 'dark-theme' class to the body
     document.body.classList.add('dark-theme');
 }
 
+// Function to set the light theme
 function setLightTheme() {
-    // Remove the 'dark-theme' class from the body
     document.body.classList.remove('dark-theme');
 }
 
-// Check if dark mode is preferred and set the theme accordingly
+// Check and set the initial theme based on user preference
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     setDarkTheme();
 } else {
     setLightTheme();
 }
 
-// Menu icon function
+// Listen for changes in color scheme preference and adjust the theme accordingly
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+    if (e.matches) {
+        setDarkTheme();
+    } else {
+        setLightTheme();
+    }
+});
+
+// Event listener for the menu icon to toggle the display of the drawer
 document.getElementById('menuIcon').addEventListener('click', function () {
-    // Toggle the display of the drawer when the menu icon is clicked
     let drawer = document.getElementById('drawer');
     drawer.style.display = (drawer.style.display === 'block') ? 'none' : 'block';
 });
 
-// Event listener for the drawer
+// Event listener for the drawer to handle clicks on specific links
 document.getElementById('drawer').addEventListener('click', function (event) {
-    // Check if the clicked element is the 'About Us' link
     if (event.target.id === 'aboutUsLink') {
         toggleAboutUs();
         document.getElementById('drawer').style.display = 'none';
     }
 
-    // Check if the clicked element is the 'Privacy Policy' link
     if (event.target.id === 'privacyPolicyLink') {
         document.getElementById('drawer').style.display = 'none';
     }
 });
 
-
-// Hide function
+// Event listener for the 'Hide' button in the 'About Us' section
 document.addEventListener('DOMContentLoaded', function () {
-    // Event listener for the 'Hide' button in the 'About Us' section
     let hideAboutUsButton = document.getElementById('hideAboutUs');
     let aboutUsSection = document.getElementById('About-Us');
 
@@ -64,66 +60,58 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-// Cookies
+// Event listener to check for user consent and display the banner if consent is not given
 document.addEventListener("DOMContentLoaded", function () {
-    // Check for user consent and display the banner if consent is not given
     if (!hasConsent()) {
         displayBanner();
     }
 });
 
+// Function to check if the user has given consent
 function hasConsent() {
-    // Check if the user has given consent by checking the 'cookieConsent' localStorage item
     return localStorage.getItem("cookieConsent") === "true";
 }
 
+// Function to set user consent
 function setConsent() {
-    // Set user consent by updating the 'cookieConsent' localStorage item to 'true'
     localStorage.setItem("cookieConsent", "true");
 }
 
+// Function to display the cookie banner
 function displayBanner() {
-    // Display the cookie banner
     let cookieBanner = document.getElementById("cookie-banner");
     cookieBanner.style.display = "block";
 }
 
+// Function to handle the 'Accept' button click, set consent, and hide the cookie banner
 function acceptCookies() {
-    // Set user consent and hide the cookie banner when 'Accept' button is clicked
     setConsent();
     setCookiePreferences();
     let cookieBanner = document.getElementById("cookie-banner");
     cookieBanner.style.display = "none";
 }
 
+// Function to handle the 'Deny' button click and hide the cookie banner
 function denyCookies() {
-    // Hide the cookie banner when 'Deny' button is clicked
     let cookieBanner = document.getElementById("cookie-banner");
     cookieBanner.style.display = "none";
 }
 
+// Function to set or remove cookies based on user preferences
 function setCookiePreferences() {
-    // Set or remove cookies based on the user's preferences
     setCookiePreference('essentialCheckbox', 'essential');
     setCookiePreference('performanceCheckbox', 'performance');
     setCookiePreference('functionalityCheckbox', 'functionality');
     setCookiePreference('thirdPartyCheckbox', 'thirdParty');
 }
 
+// Function to set or remove a specific cookie based on user preference
 function setCookiePreference(checkboxId, cookieType) {
-    // Set or remove cookies based on the user's preference
     let checkbox = document.getElementById(checkboxId);
 
     if (checkbox && checkbox.checked) {
-        // Set the cookie based on the cookieType
-        // Replace the following line with your actual code for setting cookies
         document.cookie = `${cookieType}=true; expires=Thu, 01 Jan 2030 00:00:00 UTC; path=/`;
-        console.log(`Setting ${cookieType} cookie.`);
     } else {
-        // Remove the cookie based on the cookieType
-        // Replace the following line with your actual code for removing cookies
         document.cookie = `${cookieType}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
-        console.log(`Removing ${cookieType} cookie.`);
     }
 }
-
