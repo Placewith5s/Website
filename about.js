72647165f45eff552327053ebb88bead1e96ed0c
Placewith5s <132140520+Placewith5s@users.noreply.github.com>
2023-11-24 18:28:17 +0100

    Update about.js

--- about.js
+++ about.js
@@ -1,33 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Function to check and set the theme based on user preference
-    function setTheme() {
-        const body = document.body;
-        const savedDarkMode = localStorage.getItem('dark-mode');
+// Function to check and set the theme based on user preference
+function setTheme() {
+    const body = document.body;
+    const savedDarkMode = localStorage.getItem('dark-mode');
 
-        if (savedDarkMode === 'true' || body.classList.contains('dark-theme')) {
-            body.classList.add('dark-theme');
-        }
+    if (savedDarkMode === 'true' || body.classList.contains('dark-theme')) {
+        body.classList.add('dark-theme');
     }
+}
 
-    // Check and set the theme on page load
-    setTheme();
+// Check and set the theme on page load
+setTheme();
 
-    // Function to toggle between light and dark modes
-    function toggleTheme() {
-        const body = document.body;
+// Function to toggle between light and dark modes
+function toggleTheme() {
+    const body = document.body;
 
-        // Toggle dark theme class
-        body.classList.toggle('dark-theme');
+    // Toggle dark theme class
+    body.classList.toggle('dark-theme');
 
-        // Save user preference in local storage
-        const isDarkTheme = body.classList.contains('dark-theme');
-        localStorage.setItem('dark-mode', isDarkTheme);
+    // Save user preference in local storage
+    const isDarkTheme = body.classList.contains('dark-theme');
+    localStorage.setItem('dark-mode', isDarkTheme);
+}
+
+// Listen for changes in color scheme preference and adjust the theme accordingly
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+    if (e.matches) {
+        setTheme();
     }
-
-    // Listen for changes in color scheme preference and adjust the theme accordingly
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-        if (e.matches) {
-            setTheme();
-        }
-    });
 });
