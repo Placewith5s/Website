01af3f49e16b462d079e7b833d2b724e96411823
Placewith5s <132140520+Placewith5s@users.noreply.github.com>
2023-11-28 19:41:05 +0100

    Update aboutSoftware.js

--- aboutSoftware.js
+++ aboutSoftware.js
@@ -1,22 +1,3 @@
-// Function to update the URL without page refresh
-function updateUrl() {
-    // New URL
-    var aboutSoftUrl = '/Software';
-
-    // Log to the console to check if the function is being called
-    console.log('Updating URL:', aboutSoftUrl);
-
-    // Update the URL without a page refresh
-    history.pushState({}, document.title, aboutSoftUrl);
-}
-
-// Call the function when the page loads
-window.onload = function () {
-    updateUrl();
-    // Log to the console to check if the page load event is firing
-    console.log('Page loaded');
-};
-
 // Dark mode functions
 const darkMode = (function () {
     // Private functions
