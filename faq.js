f1c3205de7baa6dda221ecb24501af195848e68a
Placewith5s <132140520+Placewith5s@users.noreply.github.com>
2023-12-05 18:32:13 +0100

    Update faq.js

--- faq.js
+++ faq.js
@@ -2,3 +2,25 @@ document.addEventListener("DOMContentLoaded", function () {
     // Call the function to check and set dark mode preference
     myDarkModeModule.checkAndSetDarkModePreference();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const searchBar = document.getElementById("search-bar");
+
+    searchBar.addEventListener("input", function () {
+        const searchTerm = searchBar.value.toLowerCase();
+
+        // Get all FAQ sections
+        const faqSections = document.querySelectorAll("main section");
+
+        // Loop through each FAQ section and toggle visibility based on the search term
+        faqSections.forEach(function (section) {
+            const sectionText = section.textContent.toLowerCase();
+
+            if (sectionText.includes(searchTerm)) {
+                section.style.display = "block";
+            } else {
+                section.style.display = "none";
+            }
+        });
+    });
+});
