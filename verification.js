cfee1e82de186e0470e942f68ddd03bcf77637ff
Placewith5s <132140520+Placewith5s@users.noreply.github.com>
2023-12-10 14:13:14 +0100

    Update verification.js

--- verification.js
+++ verification.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const siteKey = '6LfO8ikpAAAAADnNCtnMo33rJLhbLJwJzBfD0ERe';
     const verificationForm = document.getElementById("verificationForm");
-    const homePageSection = document.getElementById("Home-Page");
 
     // Retrieve verification data from cookies
     const verificationData = getVerificationDataFromCookie();
@@ -12,21 +11,23 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // Check verification status from cookies
+    const currentPage = getCurrentPage();
+    
     if (verificationData && verificationData.status === "verified" && verificationData.timestamp) {
         const currentTime = new Date().getTime();
         const thirtyMinutesInMillis = 30 * 60 * 1000;
-    
+
         if (currentTime - verificationData.timestamp >= thirtyMinutesInMillis) {
             // More than 30 minutes have passed since the last verification
-            showFormAndHideHomePage();
+            showFormAndHideContent();
         } else {
-            // Less than 30 minutes have passed, hide the form and show the Home-Page
-            hideFormAndShowHomePage();
+            // Less than 30 minutes have passed, hide the form and show the content
+            hideFormAndShowContent(currentPage);
         }
-    
+
     } else {
-        // User is not verified, hide the Home-Page and show the form
-        hideHomePageAndShowForm();
+        // User is not verified, hide the content and show the form
+        hideContentAndShowForm(currentPage);
     }
 
     verificationForm.addEventListener("submit", function (event) {
@@ -52,49 +53,54 @@ document.addEventListener("DOMContentLoaded", function () {
                     };
                     setVerificationDataCookie(newVerificationData);
 
-                    // Show the Home-Page and hide the form
-                    showHomePageAndHideForm();
+                    // Show the content and hide the form
+                    showContentAndHideForm(currentPage);
                 } else {
                     // Handle reCAPTCHA v3 verification failure
                     alert("reCAPTCHA v3 verification failed. Please try again.");
 
-                    // Hide the Home-Page and show the form
-                    hideHomePageAndShowForm();
+                    // Hide the content and show the form
+                    hideContentAndShowForm(currentPage);
                 }
             })
             .catch(function (error) {
                 console.error("Error during reCAPTCHA execution:", error);
                 alert("An error occurred during reCAPTCHA v3 verification. Please try again.");
 
-                // Hide the Home-Page and show the form
-                hideHomePageAndShowForm();
+                // Hide the content and show the form
+                hideContentAndShowForm(currentPage);
             });
     });
 
-    // Function to show the Home-Page and hide the form
-    function showHomePageAndHideForm() {
-        if (homePageSection) {
-            homePageSection.style.display = "block";
+    // Function to show the content and hide the form
+    function showContentAndHideForm(page) {
+        const contentSection = document.getElementById(page);
+        if (contentSection) {
+            contentSection.style.display = "block";
         }
         if (verificationForm) {
             verificationForm.style.display = "none";
         }
     }
 
-    // Function to hide the Home-Page and show the form
-    function hideHomePageAndShowForm() {
-        if (homePageSection) {
-            homePageSection.style.display = "none";
+    // Function to hide the content and show the form
+    function hideContentAndShowForm(page) {
+        const contentSection = document.getElementById(page);
+        if (contentSection) {
+            contentSection.style.display = "none";
         }
         if (verificationForm) {
             verificationForm.style.display = "block";
         }
     }
 
-    // Function to show the form and hide the Home-Page
-    function showFormAndHideHomePage() {
-        if (homePageSection) {
-            homePageSection.style.display = "none";
+    // Function to show the form and hide the content
+    function showFormAndHideContent() {
+        const contentSections = document.querySelectorAll('.content-section');
+        if (contentSections) {
+            contentSections.forEach(section => {
+                section.style.display = "none";
+            });
         }
         if (verificationForm) {
             verificationForm.style.display = "block";
@@ -123,5 +129,11 @@ document.addEventListener("DOMContentLoaded", function () {
     function getCookie(name) {
         const match = document.cookie.match(new RegExp(`(?:^|;\\s*)${name}=([^;]*)`));
         return match ? match[1] : null;
+    }
+
+    // Function to get the current page from the URL path
+    function getCurrentPage() {
+        const pathSegments = window.location.pathname.split('/');
+        return pathSegments[pathSegments.length - 1];
     }
 });
