87bcf828565e84441d77a00ec8e325a5c768bcc3
Placewith5s <132140520+Placewith5s@users.noreply.github.com>
2023-12-10 13:12:11 +0100

    Update verification.js

--- verification.js
+++ verification.js
@@ -1,75 +1,103 @@
+// verification.js
+
 document.addEventListener("DOMContentLoaded", function () {
-    const verificationForm = document.getElementById("myForm");
-    const mainContent = document.getElementById("main-content");
+    const siteKey = '6LfO8ikpAAAAADnNCtnMo33rJLhbLJwJzBfD0ERe';
+    const verificationForm = document.getElementById("verificationForm");
     const homePageSection = document.getElementById("Home-Page");
 
-    // Always show the badge
+    // Always show the reCAPTCHA v3 badge
     grecaptcha.ready(function () {
-        grecaptcha.execute('6LfO8ikpAAAAADnNCtnMo33rJLhbLJwJzBfD0ERe', { action: 'show' });
+        grecaptcha.execute(siteKey, { action: 'show' });
     });
 
     // Check verification status from cookies
     const verificationData = getVerificationDataFromCookie(); // Use a separate function to get verification data
 
-    if (!verificationData || !verificationData.status || !verificationData.timestamp) {
-        // Show the form only to non-verified users
-        mainContent.style.display = "block"; // Display main content
-        verificationForm.style.display = "block"; // Display the form
-        if (homePageSection) {
-            homePageSection.style.display = "none"; // Hide the Home-Page section for unverified users
-        }
-
-        verificationForm.addEventListener("submit", function (event) {
-            event.preventDefault();
-
-            // Execute and get the token
-            grecaptcha.execute('6LfO8ikpAAAAADnNCtnMo33rJLhbLJwJzBfD0ERe', { action: 'submit' })
-                .then(function (token) {
-                    // For demonstration purposes, log the token to the console
-                    console.log("reCAPTCHA Token:", token);
-
-                    // Simulate verification logic
-                    const isVerified = token && token.length > 0;
-
-                    if (isVerified) {
-                        mainContent.style.display = "none";
-                        verificationForm.style.display = "none";
-                        if (homePageSection) {
-                            homePageSection.style.display = "block"; // Show the Home-Page section for verified users
-                        }
-                        const newVerificationData = {
-                            status: "verified",
-                            timestamp: new Date().getTime()
-                        };
-                        setVerificationDataCookie(newVerificationData); // Set a cookie with verification data
-                        alert("Successfully verified!");
-                    } else {
-                        alert("Verification failed. Please try again.");
-                    }
-                })
-                .catch(function (error) {
-                    console.error("Error during reCAPTCHA execution:", error);
-                    alert("An error occurred during reCAPTCHA verification. Please try again.");
-                });
-        });
-    } else {
-        // Check if the verification was within the last 30 minutes
+    if (verificationData && verificationData.status === "verified" && verificationData.timestamp) {
         const currentTime = new Date().getTime();
         const thirtyMinutesInMillis = 30 * 60 * 1000;
 
-        if (currentTime - verificationData.timestamp < thirtyMinutesInMillis) {
-            // Hide the entire content for verified users
-            mainContent.style.display = "none";
-            if (homePageSection) {
-                homePageSection.style.display = "block"; // Show the Home-Page section for verified users
-            }
+        if (currentTime - verificationData.timestamp >= thirtyMinutesInMillis) {
+            // More than 30 minutes have passed since the last verification
+            showFormAndHideHomePage();
         } else {
-            // Verification has expired, show the form again
-            mainContent.style.display = "block";
+            // Less than 30 minutes have passed, hide the form and show the Home-Page
+            hideFormAndShowHomePage();
+        }
+    } else {
+        // User is not verified, hide the Home-Page and show the form
+        hideHomePageAndShowForm();
+    }
+
+    verificationForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        // Execute and get the reCAPTCHA token
+        grecaptcha.execute(siteKey, { action: 'submit' })
+            .then(function (token) {
+                // For demonstration purposes, log the reCAPTCHA token to the console
+                console.log("reCAPTCHA Token:", token);
+
+                // Simulate verification logic
+                const isVerified = token && token.length > 0;
+
+                if (isVerified) {
+                    // Handle successful reCAPTCHA v3 verification
+                    alert("Successfully verified with reCAPTCHA v3!");
+
+                    // Set a new verification timestamp in the cookie
+                    const newVerificationData = {
+                        status: "verified",
+                        timestamp: new Date().getTime()
+                    };
+                    setVerificationDataCookie(newVerificationData);
+
+                    // Show the Home-Page and hide the form
+                    showHomePageAndHideForm();
+                } else {
+                    // Handle reCAPTCHA v3 verification failure
+                    alert("reCAPTCHA v3 verification failed. Please try again.");
+
+                    // Hide the Home-Page and show the form
+                    hideHomePageAndShowForm();
+                }
+            })
+            .catch(function (error) {
+                console.error("Error during reCAPTCHA execution:", error);
+                alert("An error occurred during reCAPTCHA v3 verification. Please try again.");
+
+                // Hide the Home-Page and show the form
+                hideHomePageAndShowForm();
+            });
+    });
+
+    // Function to show the Home-Page and hide the form
+    function showHomePageAndHideForm() {
+        if (homePageSection) {
+            homePageSection.style.display = "block";
+        }
+        if (verificationForm) {
+            verificationForm.style.display = "none";
+        }
+    }
+
+    // Function to hide the Home-Page and show the form
+    function hideHomePageAndShowForm() {
+        if (homePageSection) {
+            homePageSection.style.display = "none";
+        }
+        if (verificationForm) {
             verificationForm.style.display = "block";
-            if (homePageSection) {
-                homePageSection.style.display = "none";
-            }
+        }
+    }
+
+    // Function to show the form and hide the Home-Page
+    function showFormAndHideHomePage() {
+        if (homePageSection) {
+            homePageSection.style.display = "none";
+        }
+        if (verificationForm) {
+            verificationForm.style.display = "block";
         }
     }
 
@@ -83,17 +111,17 @@ document.addEventListener("DOMContentLoaded", function () {
         const verificationData = getCookie("verificationData");
         return verificationData ? JSON.parse(verificationData) : null;
     }
+
+    // Function to set a cookie
+    function setCookie(name, value, days) {
+        const expires = new Date();
+        expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+        document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+    }
+
+    // Function to get a cookie value by name
+    function getCookie(name) {
+        const match = document.cookie.match(new RegExp(`(?:^|;\\s*)${name}=([^;]*)`));
+        return match ? match[1] : null;
+    }
 });
-
-// Function to set a cookie
-function setCookie(name, value, days) {
-    const expires = new Date();
-    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
-}
-
-// Function to get a cookie value by name
-function getCookie(name) {
-    const match = document.cookie.match(new RegExp(`(?:^|;\\s*)${name}=([^;]*)`));
-    return match ? match[1] : null;
-}
