5bc23f18be5704fde535a0b9506b0701e830da39
Placewith5s <132140520+Placewith5s@users.noreply.github.com>
2023-12-10 14:54:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,7 @@
-// Dark mode functionality
-const darkMode = (function () {
-    // Private functions for dark mode
-    function setDarkMode() {
-        document.body.classList.add('dark-theme');
-    }
-
-    function setLightMode() {
-        document.body.classList.remove('dark-theme');
-    }
-
-    function toggleDarkMode() {
-        const isDarkMode = document.body.classList.contains('dark-theme');
-        isDarkMode ? setLightMode() : setDarkMode();
-    }
-
-    // Check for dark mode preference and set the theme accordingly
-    function checkAndSetDarkModePreference() {
-        try {
-            if (window.matchMedia) {
-                const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-
-                // If the user prefers dark mode, set the dark theme
-                if (darkModeMediaQuery.matches) {
-                    setDarkMode();
-                }
-            } else {
-                // Log an error if matchMedia is not supported
-                console.error('matchMedia is not supported. Dark mode preference may not work.');
-            }
-        } catch (error) {
-            // Log an error if an exception occurs during dark mode preference check
-            console.error('An error occurred while checking dark mode preference:', error);
-        }
-    }
-
-    // Expose public functions
-    return {
-        setDarkMode,
-        setLightMode,
-        toggleDarkMode,
-        checkAndSetDarkModePreference
-    };
-})();
+document.addEventListener("DOMContentLoaded", function () {
+    // Call the function to check and set dark mode preference
+    myDarkModeModule.checkAndSetDarkModePreference();
+});
 
 const MenuManager = {
     // Toggle the drawer's display
@@ -86,6 +46,5 @@ const MenuManager = {
     }
 };
 
-// Initialize modules
-darkMode.checkAndSetDarkModePreference();
+// Initialize module
 MenuManager.initializeMenu();
