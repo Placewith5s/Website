38c51cc80845c9ebf5a1b32aff99c8b87d93457c
Placewith5s <132140520+Placewith5s@users.noreply.github.com>
2023-12-12 11:04:46 +0100

    Update faq.js

--- faq.js
+++ faq.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener("DOMContentLoaded", function () {
     const searchBar = document.getElementById("search-bar");
+    const notFoundMessage = document.getElementById("not-found-message");
 
     searchBar.addEventListener("input", function () {
         const searchTerm = searchBar.value.toLowerCase();
@@ -12,15 +13,21 @@ document.addEventListener("DOMContentLoaded", function () {
         // Get all FAQ sections
         const faqSections = document.querySelectorAll("main section");
 
+        let found = false;
+
         // Loop through each FAQ section and toggle visibility based on the search term
         faqSections.forEach(function (section) {
             const sectionText = section.textContent.toLowerCase();
 
             if (sectionText.includes(searchTerm)) {
                 section.style.display = "block";
+                found = true;
             } else {
                 section.style.display = "none";
             }
         });
+
+        // Display or hide the "not found" message based on whether any section was found
+        notFoundMessage.style.display = found ? "none" : "block";
     });
 });
