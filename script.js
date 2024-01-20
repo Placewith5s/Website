d382147dc17e8e87d5d972d7a0b974f77224beff
Placewith5s <132140520+Placewith5s@users.noreply.github.com>
2024-01-20 10:27:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -64,24 +64,3 @@ const MenuManager = {
 };
 
 MenuManager.initializeMenu();
-
-
-document.getElementById('Expand-Collapse-Button').addEventListener('click', toggleElements);
-
-function toggleElements() {
-    const firstElement = document.getElementById('step-hidden-from-5');
-
-    if (firstElement) {
-        const isHidden = window.getComputedStyle(firstElement).display === 'none';
-
-        for (let i = 5; i <= 12; i++) {
-            const element = document.getElementById(`step-hidden-from-${i}`);
-            if (element) {
-                element.style.display = isHidden ? 'block' : 'none';
-            }
-        }
-
-        const button = document.getElementById('Expand-Collapse-Button');
-        button.innerText = isHidden ? 'Expand' : 'Collapse';
-    }
-}
