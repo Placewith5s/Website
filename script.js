aaaf9d660a260dab6a47dcba11b32f7776058bba
Placewith5s <132140520+Placewith5s@users.noreply.github.com>
2024-01-30 13:57:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 document.addEventListener("DOMContentLoaded", function () {
     // Call the function to check and set dark mode preference
     myDarkModeModule.checkAndSetDarkModePreference();
