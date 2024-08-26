6cd1af70a6f18f76a964c6e7a7e3ce5b4571e4b6
Placewith5s <qsenkaisu@gmail.com>
2024-08-26 18:25:22 +0200

    fixes and updates improved header fixed 'use strict' not used inside the IIFE will fix performance for event listeners

--- js/faq.js
+++ js/faq.js
@@ -1,3 +1,3 @@
-'use strict';(function(){class SummaryElement{constructor(){this.element=document.querySelector('summary');this.element.setAttribute('aria-expanded','false');this.element.addEventListener('click',()=>{this.toggleExpanded()},{passive:!0})}
-toggleExpanded(){const isExpanded=this.element.getAttribute('aria-expanded')==='true';this.element.setAttribute('aria-expanded',!isExpanded)}}
+(function(){"use strict";class SummaryElement{constructor(){this.element=document.querySelector("summary");this.element.setAttribute("aria-expanded","false");this.element.addEventListener("click",()=>{this.toggleExpanded()},{passive:!0})}
+toggleExpanded(){const isExpanded=this.element.getAttribute("aria-expanded")==="true";this.element.setAttribute("aria-expanded",!isExpanded)}}
 const summary=new SummaryElement()})()
